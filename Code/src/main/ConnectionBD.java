1c66584d1fbfbb3cff281255b8a4015f2b83c011
Michele M <46691861+michelicious11@users.noreply.github.com>
2021-11-17 13:43:06 -0500

    gros refactoring2, bye Netbeans, admin interface finie, connexion db

--- Code/src/main/ConnectionBD.java
+++ Code/src/main/ConnectionBD.java
@@ -1,31 +1,90 @@
 package main;
 
+import java.sql.Statement;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.sqlite.SQLiteDataSource;
 
+import locosys.views.AdminPages;
+
 public class ConnectionBD {
 
-    public static void main(String[] args) {
-        SQLiteDataSource ds = null;
+	public static void main(String[] args) {
+		SQLiteDataSource ds = null;
 
-        try {
-            ds = new SQLiteDataSource();
-            ds.setUrl("jdbc:sqlite:locar-xpress.db");
-        } catch ( Exception e ) {
-            e.printStackTrace();
-            System.exit(0);
-        }
-        System.out.println( "Opened database successfully" );
+		try {
+			getSqlCommands();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} 
 
-        try ( Connection conn = ds.getConnection() ) {
-        } catch ( SQLException e ) {
-            e.printStackTrace();
-            System.exit( 0 );
-        }
+		try {
+			ds = new SQLiteDataSource();
+			ds.setUrl("jdbc:sqlite:locar-xpress.db");
+		} catch ( Exception e ) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+		System.out.println( "Opened database successfully" );
 
-        System.out.println( "Created database successfully" );
-    }
+		//String query = "CREATE TABLE IF NOT EXISTS userTest ( " +
+		//"ID INTEGER PRIMARY KEY, "
+		//	+ "NAME TEXT NOT NULL)";
+		//String query2 = "INSERT INTO userTest ( NAME) VALUES " +
+		//	"'Shane'";
+		String query3 = "SELECT * FROM userTest";
 
-}
+		try ( Connection conn = ds.getConnection();
+				Statement stmt = conn.createStatement();) {
+
+			//int rv = stmt.executeUpdate(query);
+			//int rv2 = stmt.executeUpdate(query2);
+			ResultSet rs = stmt.executeQuery(query3);
+
+			while ( rs.next() ) {
+				int id = rs.getInt( "ID" );
+				String name = rs.getString( "NAME" );
+
+				System.out.println("Result: ID = " + id + ", NAME = " + name );
+			}
+
+
+		} catch ( SQLException e ) {
+			e.printStackTrace();
+			System.exit( 0 );
+		}
+ 
+		System.out.println( "Created database successfully" );
+	}
+	
+	public static void getSqlCommands() throws IOException {
+		URL fileContent = ConnectionBD.class.getResource("/text/sqlCommands.txt");
+		//String sqlCommands = Files.readString(fileContent);
+		//try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileContent))){
+			
+			char[] array = new char[10];
+			//read into array
+			//bufferedReader.read(array, 0, 5);
+			for (char c : array) {
+				 // if char is empty
+	            if(c == (char)0) {
+	               c = '*';
+	            }
+	            // prints characters
+	            System.out.print(c);
+			}
+		//} catch (Exception e) {
+			//e.printStackTrace();
+		}
+	}
+
+
+//}
--- Code/src/main/main.java
+++ Code/src/main/main.java
@@ -1,8 +1,25 @@
 package main;
 
+import java.awt.EventQueue;
+
+import locosys.views.AdminPages;
 
 public class main {
 
-
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AppLogin window = new AppLogin();
+					window.getFrame().setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
     
 }
